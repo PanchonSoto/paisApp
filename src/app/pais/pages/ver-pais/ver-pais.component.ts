fbc19ad79a733ae13f4b5d9119bf25f85db0ae1e
Pancho Soto <panchon.ues@gmail.com>
2021-09-07 17:04:41 -0700

    verPais Page

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,18 +12,31 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country
+  translations!: string[]
+
   constructor(private actRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
 
     this.actRoute.params
+      .pipe(
+        switchMap(({ id })=> this.paisService.getPaisId( id )),
+        tap(console.log)
+      )
+      .subscribe(pais=>{
+        this.pais = pais;
+        this.translations = Object.values( pais.translations );
+      });
+
+    /* this.actRoute.params
       .subscribe(( {id} )=>{
         console.log(id);
         this.paisService.getPaisId(id)
           .subscribe(pais=>{
             console.log(pais);
           })
-      });
+      }); */
   }
 
 }
